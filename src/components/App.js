0228d4630a95b8a6b62038466e540adbcc2e70a0
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-11 00:53:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { useState } from "react";
+import "./styles.css";
 function App() {
-  const [booli, setbooli] = useState(false);
-  const handleButton = () => {
-    setbooli(true);
-  };
-  return (
-    <div id="main">
-      {booli === false ? (
-        <button id="click" onClick={handleButton}>
-          button
-        </button>
-      ) : (
+  const [para, setPara] = useState(false);
+  function clickHandler() {
+    setPara(true);
+  }
+
+  function hideParagraph() {
+    if (para) {
+      return (
         <p id="para">
-          {
-            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-          }
+          {"Hello, I've learnt to use the full-stack evaluation tool. This makesme so happy"}
         </p>
-      )}
-    </div>
-  );
+      );
+    } else {
+      return (
+        <div id="main">
+          <button id="click" onClick={clickHandler}>
+            click me
+          </button>
+        </div>
+      );
+    }
+  }
+  return <div id="para">{hideParagraph()}</div>;
 }
-
 export default App;
-
